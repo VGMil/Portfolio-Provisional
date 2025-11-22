70793779acf87cae267dea89486490b40fb3da20
VGMil <vgmiltonisaac@gmail.com>
2025-11-22 15:12:18 -0500

    ✨ feat(ui): Implementar diseño inicial de portafolio    - Crear la estructura principal de la página con navegación y tarjeta 3D.    - Añadir estilos base para la nueva interfaz de usuario.    - Incluir imagen de avatar para el perfil.

--- src/index.php
+++ src/index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portfolio VGMil</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+<nav class="nav-console">
+    <div class="nav-brand">VGMIL</div>
+    <ul class="nav-menu">
+        <li><a href="index.php" class="<?php echo ($pagina_actual == 'index.php') ? 'active' : ''; ?>">Inicio</a></li>
+        <li><a href="contacto.php" class="<?php echo ($pagina_actual == 'contacto.php') ? 'active' : ''; ?>">Contacto</a></li>
+    </ul>
+</nav>
+
+<main>
+    <section class="flat-card">
+        
+        <img src="./assets/me.jpeg" alt="Milton Velásquez" class="avatar-3d">
+
+        <div class="content-wrapper">
+            <div class="title-card">
+                <h1>@VGMIL / Portfolio </h1>
+                <h2><strong>Full Stack Developer</strong> | Java, Spring, React, React Native, Firebase | IoT & AI Solutions</h2>
+            </div>
+            <div class="content-card">
+                <p>Me distingo por ir más allá del código tradicional:</p>
+                <p>Integro soluciones <strong>IoT</strong> y <strong>AI</strong> para resolver problemas reales.</p>
+                <p>Busco desafíos en entornos donde pueda aplicar <strong>metodologías Ágiles</strong>, principios <strong>SOLID</strong> y crear tecnología que <strong>marquen la diferencia</strong>.</p>
+            </div>
+            <button class='button-card'>
+                ¡Contáctame!
+            </button>
+        </div>
+    </section>
+</main>
+
 </body>
 </html>
